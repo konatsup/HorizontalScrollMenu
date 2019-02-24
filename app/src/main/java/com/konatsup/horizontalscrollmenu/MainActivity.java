bb96bce1b3f01c497ef8f1bbdd580d830ac88c34
konatsup <konatsukomatsu@gmail.com>
2019-02-24 11:55:04 +0900

    パーケージ名を全て取得しListViewに表示

--- app/src/main/java/com/konatsup/horizontalscrollmenu/MainActivity.java
+++ app/src/main/java/com/konatsup/horizontalscrollmenu/MainActivity.java
@@ -1,13 +1,36 @@
 package com.konatsup.horizontalscrollmenu;
 
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listView);
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1);
+
+        //PackageManagerのオブジェクトを取得
+        PackageManager pm = this.getPackageManager();
+
+        //インストール済パッケージ情報を取得する
+        List<ApplicationInfo> list = pm.getInstalledApplications(0);
+
+        //パッケージ情報をリストビューに追記
+        for (ApplicationInfo ai : list) {
+            arrayAdapter.add(ai.packageName);
+            listView.setAdapter(arrayAdapter);
+        }
     }
 }
